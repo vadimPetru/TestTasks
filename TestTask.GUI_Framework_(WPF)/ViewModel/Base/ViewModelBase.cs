537a5872bf54f321cb04bedaf67ea12678825efa
Vadim <6ddd14@gmail.com>
2025-02-27 14:43:20 +0300

    Created ViewModelBase

---
+++ TestTask.GUI_Framework_(WPF)/ViewModel/Base/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TestTask.GUI_Framework__WPF_.ViewModel.Base;
+
+
+/// <summary>
+/// Базовый класс для всех ViewModel ,позволяет уведомлять UI об изменениях
+/// </summary>
+internal abstract class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+    }
+
+    protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+    {
+        if (Equals(field, value)) return false;
+
+        field = value;
+        OnPropertyChanged(PropertyName);
+        return true;
+    }
+}
